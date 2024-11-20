325fd50f1da5a7384be99c9a18b6a7bfc7892793
Esma Çokyüksel <esmacokyukse@gmail.com>
2024-11-20 05:46:48 +0300

    Add project files.

---
+++ SignalR.DataAccessLayer/EntityFramework/EfProductDal.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntiyLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+	public class EfProductDal : GenericRepository<Product>, IProductDal
+	{
+		public EfProductDal(SignalRContext context) : base(context)
+		{
+		}
+
+	}
+}
